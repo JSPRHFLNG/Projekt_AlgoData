ad3661335e644217d475899927ecd32955a50c77
GitCodeChris <gitcodechris@outlook.com>
2025-05-19 23:09:07 +0200

    Update Graph.java

--- src/model/graph/Graph.java
+++ src/model/graph/Graph.java
@@ -1,9 +1,24 @@
 package model.graph;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Graph<T> implements GraphInterface<T>
 {
+    private int nVertices;
+    private int nEdges;
+    private HashMap<T, Vertex<T>> vertices;
+    private HashMap<T, ArrayList<Edge<T>>> edges;
+
+    public Graph()
+    {
+        vertices = new HashMap<>();
+        edges = new HashMap<>();
+        nVertices = 0;
+        nEdges = 0;
+    }
+
 
     /**
      * Extracts and returns a list of all vertices stored in the graph
@@ -11,9 +26,11 @@ public class Graph<T> implements GraphInterface<T>
      * @return a list containing all {@link Vertex} objects
      */
     @Override
-    public List<Vertex<T>> getAllVertices() {
-        return List.of();
+    public List<Vertex<T>> getAllVertices()
+    {
+        return new ArrayList<>(vertices.values());
     }
+
 
     /**
      * Extracts and returns all edges connected to the vertex identified
@@ -24,27 +41,50 @@ public class Graph<T> implements GraphInterface<T>
      * the vertex
      */
     @Override
-    public List<Edge<T>> getEdges(Object info) {
-        return List.of();
+    public List<Edge<T>> getEdges(T info)
+    {
+        return edges.getOrDefault(info, new ArrayList<>());
     }
+
 
     /**
      * Adds a new {@link Vertex} to the graph at the given position
+     * Does not allow duplicates.
+     * @param x the x coordinate.
+     * @param y the y coordinate.
+     * @param info the identifier of the vertex
+     */
+    @Override
+    public void addVertex(double x, double y,T info)
+    {
+        if(!vertices.containsKey(info)){
+            vertices.put(info, new Vertex<>(x, y, info));
+            edges.put(info, new ArrayList<>());
+            nVertices++;
+        }
+    }
+
+
+    /**
+     * Adds a new {@link Vertex} to the graph at the given position.
+     * Does not allow duplicates.
      *
-     * @param x
-     * @param y
-     * @param z
+     * @param x the x coordinate.
+     * @param y the y coordinate.
+     * @param z the z coordinate.
      * @param info the information and identifier of the vertex
      */
     @Override
-    public void addVertex(double x, double y, double z, Object info) {
-
+    public void addVertex(double x, double y, double z,T info)
+    {
+        if(!vertices.containsKey(info))
+        {
+            vertices.put(info, new Vertex<>(x, y, z,info));
+            edges.put(info, new ArrayList<>());
+            nVertices++;
+        }
     }
 
-    @Override
-    public void addVertex(double x, double y, Object info) {
-
-    }
 
     /**
      * Adds an undirected edge between two vertices, internally stored as
@@ -54,9 +94,23 @@ public class Graph<T> implements GraphInterface<T>
      * @param infoB the identifier of vertex B
      */
     @Override
-    public void addEdge(Object infoA, Object infoB) {
+    public void addEdge(T infoA, T infoB)
+    {
+        if(infoA == null || infoB == null){return;}
 
+        if(vertices.containsKey(infoA) && vertices.containsKey(infoB))
+        {
+            Vertex<T> vertexA = vertices.get(infoA);
+            Vertex<T> vertexB = vertices.get(infoB);
+            Edge<T> edgeAB = new Edge<>(vertexA, vertexB);
+            Edge<T> edgeBA = new Edge<>(vertexB, vertexA);
+
+            edges.get(infoA).add(edgeAB);
+            edges.get(infoB).add(edgeBA);
+            nEdges++;
+        }
     }
+
 
     /**
      * Removes the {@link Vertex} object identified by info, and all
@@ -65,9 +119,25 @@ public class Graph<T> implements GraphInterface<T>
      * @param info the identifier of the vertex to remove
      */
     @Override
-    public void remove(Object info) {
+    public void remove(T info)
+    {
+        if(!vertices.containsKey(info) || info == null)
+        {
+            return;
+        }
 
+        nEdges -= edges.get(info).size() / 2;
+        for(T key : edges.keySet())
+        {
+            edges.get(key).
+                    removeIf(edge->
+                            edge.getTo().getInfo().equals(info));
+        }
+        vertices.remove(info);
+        edges.remove(info);
+        nVertices--;
     }
+
 
     /**
      * Returns the number of {@link Edge} objects in the graph. One double edge
@@ -76,9 +146,11 @@ public class Graph<T> implements GraphInterface<T>
      * @return number of edges
      */
     @Override
-    public int numberOfEdges() {
-        return 0;
+    public int numberOfEdges()
+    {
+        return nEdges;
     }
+
 
     /**
      * Returns the number of {@link Vertex} objects in the graph
@@ -86,7 +158,9 @@ public class Graph<T> implements GraphInterface<T>
      * @return number of vertices
      */
     @Override
-    public int numberOfVertices() {
-        return 0;
+    public int numberOfVertices()
+    {
+        return nVertices;
     }
+
 }
